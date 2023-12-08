c02a7e759976c5956b92eb40fa0371ce3c822aed
Furkan Esen <furkanesen35@gmail.com>
2023-12-08 11:47:21 +0100

    checkpoint

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -4,11 +4,14 @@ import foto from "./foto.jpg"
 const History = () => {
  return (
   <div className='flex justify-center'>
-   <div className='flex flex-col justify-center items-center w-[60%] sm:flex-row m-[40px]'>
+   <div className='flex flex-col justify-center items-center w-[60%] md:flex-row m-[40px] mt-[100px]'>
     <div className=''>
      <img className='w-[350px]' src={foto} alt="" />
     </div>
-    <div className='w-[600px] p-[15px]'>Hi</div>
+    <div className='flex flex-col md:w-[600px] p-[15px]'>
+     <h1 className='text-[40px]'>Hallo! Ich bin</h1>
+     <h2 className='text-[60px]'>Furkan Esen</h2>
+    </div>
    </div>
   </div>
  )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,9 +17,9 @@ export default function Example() {
   <Disclosure as="nav" className="bg-gray-800 fixed w-[100%]">
    {({ open }) => (
     <>
-     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+     <div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
       <div className="relative flex h-16 items-center justify-center">
-       <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+       <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
         {/* Mobile menu button*/}
         <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
          <span className="absolute -inset-0.5" />
@@ -31,11 +31,11 @@ export default function Example() {
          )}
         </Disclosure.Button>
        </div>
-       <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-center">
+       <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-center">
         <div className="flex flex-shrink-0 items-center text-white cursor-pointer">
          Furkan Esen
         </div>
-        <div className="hidden sm:ml-6 sm:block">
+        <div className="hidden md:ml-6 md:block">
          <div className="flex space-x-4">
           {navigation.map((item) => (
            <a
@@ -55,7 +55,7 @@ export default function Example() {
        </div>
       </div>
      </div>
-     <Disclosure.Panel className="sm:hidden">
+     <Disclosure.Panel className="md:hidden">
       <div className="space-y-1 px-2 pb-3 pt-2">
        {navigation.map((item) => (
         <Disclosure.Button
